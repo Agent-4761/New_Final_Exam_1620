08590830a41bbeac7a9ec292a8bc304a241c41ca
Anjana <anjananeharaj@gmail.com>
2021-12-10 12:56:56 -0800

    trying to make dark-mode work

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,17 @@
-// function mode_swap(event) {
-// 	eleList = document.getElementsByTagName("*");
+function mode_swap(event) {
+	eleList = document.getElementsByTagName("*");
 
-// 	for (let i = 0; i < eleList.length; i++) {
-// 		classList = eleList[i].classList;
-// 		if (!classList.contains("light-theme")) {
-// 			classList.add("light-theme");
-// 			classList.remove("dark-theme");
-// 		} else {
-// 			classList.remove("light-theme");
-// 			classList.add("dark-theme");
-// 		}
-// 	}
-// }
+	for (let i = 0; i < eleList.length; i++) {
+		classList = eleList[i].classList;
+		if (!classList.contains("light-theme")) {
+			classList.add("light-theme");
+			classList.remove("dark-theme");
+		} else {
+			classList.remove("light-theme");
+			classList.add("dark-theme");
+		}
+	}
+}
 
 // make_form();
 // formButton = document.querySelector("#input");
